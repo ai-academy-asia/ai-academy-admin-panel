341252cb9476052d3cc7060c3d26cf7c1521c430
bataatsets <bataa.tsets@gmail.com>
2024-11-20 01:11:51 +0800

    updated ..

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       exec_mode: 'cluster',
       instances: '2',
       script: './node_modules/nuxt/bin/nuxt.js',
-      max_memory_restart: '100M',
+      max_memory_restart: '200M',
       args: 'start',
       env: {
         NODE_ENV: 'production'
