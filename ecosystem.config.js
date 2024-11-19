2e8959a6efb4a334dcedce6d5aea1132b07ac912
bataatsets <bataa.tsets@gmail.com>
2024-11-20 01:05:29 +0800

    updated ..

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,9 @@ module.exports = {
     {
       name: 'wecode-admin',
       exec_mode: 'cluster',
-      instances: '4',
+      instances: '2',
       script: './node_modules/nuxt/bin/nuxt.js',
-      max_memory_restart: '500M',
+      max_memory_restart: '100M',
       args: 'start',
       env: {
         NODE_ENV: 'production'
