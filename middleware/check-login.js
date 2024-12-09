4d92e11341ef20a5507126803ded4a2bfb8ac67c
bataatsets <bataa.tsets@gmail.com>
2024-12-09 19:38:24 +0800

    updated ..

--- middleware/check-login.js
+++ middleware/check-login.js
@@ -1,9 +1,9 @@
-export default ({ redirect, query, req, route }) => {
+export default ({ redirect, query, req, route, params }) => {
   const host = req.headers.host
   const allowedDomains = ['localhost:3060', 'eshop.pearls.mn']
   if (allowedDomains.includes(host) && route.name === 'login') {
-    return redirect({ name: 'shop-login', query })
+    return redirect({ name: 'shop-login', query, params })
   } else if (!allowedDomains.includes(host) && route.name === 'shop-login') {
-    return redirect({ name: 'login', query })
+    return redirect({ name: 'login', query, params })
   }
 }
